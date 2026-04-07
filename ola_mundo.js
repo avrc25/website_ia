25ddade2f9632544015c4152c6975ad4caa1099f
avrc25 <consultor.avrc@gmail.com>
2026-04-07 19:07:07 -0300

    Add files via upload

---
+++ ola_mundo.js
@@ -0,0 +1,24 @@
+//console.log("Olá Mundo em java script")
+//console.log("Vamos merendar! Tô que não me aguento")
+//alert("Fui")
+
+let nome = 'André';
+let idade = 35;
+let preco = 7.2
+var legado="Evite usar"; //Forma mais antiga
+let anoNascimento = 1991;
+let anoAtual = 2026;
+const PI = 3.14159;
+
+console.log(nome + ", sua idade é: ", anoAtual - anoNascimento, )
+let valor1 = 2;
+let valor2 = 30;
+console.log("Operadores aritiméticos +,-, /, %, e *")
+
+consule.log(`Adição: ${valor2 + valor1}')
+consule.log(`Incrementa: ${valor2 ++ valor1}`)// shift + alt + seta para baixo
+consule.log(`Subtração:, ${valor2 - valor1}`)
+consule.log(`Divisão:, ${valor2 / valor1}`)
+consule.log(`Exponenciação:, ${valor2 ** valor1}`)
+consule.log(`Módulo:, ${valor2 % valor1}`)
+consule.log(`Multiplicação:, ${valor2 * valor1}`)
